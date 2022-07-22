1e0ffbd9ca064d3f299f13f5564660f07c03bb68
Dancurvello <curvellodaniel@gmail.com>
2022-07-22 12:31:19 -0300

    primeiro commit

---
+++ app.js
@@ -0,0 +1,21 @@
+'use strict'
+
+// Adicionando um manipulador de eventos
+//Manipulador de eventos funciona com o click do mouse
+
+
+// fazendo referência ao botão
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme')
+
+    var className = document.body.className;
+    if(className == "light-theme")
+    {this.textContent = "Dark";}
+    else {
+        this.textContent = "Light";
+    }
+    console.log('current class name: ' + className);
+});
+
